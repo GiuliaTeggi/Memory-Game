a5c90d5a4e9e2e9fc05d02123ed7ae2fd6dcc9de
Giulia Teggi <teggi.giulia@gmail.com>
2021-12-22 19:46:20 +0000

    Add images util file

---
+++ src/utils/images.js
@@ -0,0 +1,15 @@
+import ape from '../../public/images/ape.jpg';
+import beeEater from '../../public/images/bee-eater.jpg';
+import elephant from '../../public/images/elephant.jpg';
+import flamingo from '../../public/images/flamingo.jpg';
+import frog from '../../public/images/frog.jpg';
+import pelican from '../../public/images/pelican.jpg';
+import rhino from '../../public/images/rhino.jpg';
+import shark from '../../public/images/shark.jpg';
+import tiger from '../../public/images/tiger.jpg';
+import zebra from '../../public/images/zebra.jpg';
+
+const images = [ape, beeEater, elephant, flamingo, frog,
+  pelican, rhino, shark, tiger, zebra];
+
+export default images;
