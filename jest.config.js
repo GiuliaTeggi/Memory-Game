6dc40dd733de47447263f4686eaae2b2e6dc6970
Giulia Teggi <teggi.giulia@gmail.com>
2021-12-24 00:21:25 +0000

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    '\\.(css)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(scss|css)$': '<rootDir>/__mocks__/styleMock.js',
     '\\.(jpg)$': '<rootDir>/__mocks__/fileMock.js',
   },
 };
