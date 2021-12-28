821f9493b73e0840d79010d63a02492729105749
Giulia Teggi <teggi.giulia@gmail.com>
2021-12-28 19:15:12 +0000

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,55 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: path.join(__dirname, '/src/index.js'),
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: ['style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              additionalData: '@import "src/styles/variables.scss";',
-            },
-          }],
-      },
-      {
-        test: /\.jpg/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.svg/,
-        type: 'asset/inline',
-      },
-    ],
-  },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    static: path.join(__dirname, '/dist'),
-    port: process.env.PORT || 3000,
-    hot: true,
-  },
-  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+module.exports = (env) => {
+  const isProdBuild = env.production;
+  const styleLoader = isProdBuild ? MiniCssExtractPlugin.loader : 'style-loader';
+
+  return {
+    entry: path.join(__dirname, '/src/index.js'),
+    mode: isProdBuild ? 'production' : 'development',
+    devtool: isProdBuild ? 'source-map' : 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            styleLoader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                additionalData: '@import "src/styles/variables.scss";',
+              },
+            }],
+        },
+        {
+          test: /\.jpg/,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.svg/,
+          type: 'asset/inline',
+        },
+      ],
+    },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'bundle.js',
+      clean: true,
+    },
+    devServer: {
+      static: path.join(__dirname, '/dist'),
+      port: process.env.PORT || 3000,
+      hot: true,
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './public/index.html' }), new MiniCssExtractPlugin()],
+  };
 };
